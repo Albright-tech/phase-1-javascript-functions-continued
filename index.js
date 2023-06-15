55db29823e34ee859d44c50c6483b64495240187
Albright <albright.muchori@student.moringaschool.com>
2023-06-15 08:41:43 +0300

    functions

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 // code your solution here
-function saturdayFun(activity = 'roller-skate') {
-    return `On Saturdays, I like to ${activity}!`;
-  }
-  function saturdayFun(activity = 'roller-skate') {
-    return `This Saturday, I want to ${activity}!`;
-  }
-  
-  module.exports = saturdayFun; 
- function mondayWork(task = 'go to the office') {
-    return `On Mondays, I need to ${task}!`;
-  }
-  function mondayWork(task = 'go to the office') {
-    return `This Monday, I will ${task}.`;
+function saturdayFun(activity) {
+    if (activity === undefined) {
+      return 'This Saturday, I want to roller-skate!';
+    } else {
+      return `This Saturday, I want to ${activity}!`;
+    }
   }
   
-  module.exports = {
-    saturdayFun: saturdayFun,
-    mondayWork: mondayWork
-  };
-  function wrapAdjective(flair = "*") {
+  function mondayWork(task) {
+    if (task === undefined) {
+      return 'This Monday, I will go to the office.';
+    } else {
+      return `This Monday, I will ${task}.`;
+    }
+  }
+  
+  function wrapAdjective(flair = '*') {
     return function (adjective) {
       return `You are ${flair}${adjective}${flair}!`;
     };
   }
   
-  module.exports = {
-    saturdayFun: saturdayFun,
-    mondayWork: mondayWork,
-    wrapAdjective: wrapAdjective
-  };
+ 
+  
   

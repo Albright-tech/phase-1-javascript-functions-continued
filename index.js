e18c5b007765a20cdb07094830b923a5a1fcf586
Albright <albright.muchori@student.moringaschool.com>
2023-06-15 08:32:50 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `On Saturdays, I like to ${activity}!`;
+  }
+  function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  module.exports = saturdayFun; 
+ function mondayWork(task = 'go to the office') {
+    return `On Mondays, I need to ${task}!`;
+  }
+  function mondayWork(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+  }
+  
+  module.exports = {
+    saturdayFun: saturdayFun,
+    mondayWork: mondayWork
+  };
+  function wrapAdjective(flair = "*") {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  module.exports = {
+    saturdayFun: saturdayFun,
+    mondayWork: mondayWork,
+    wrapAdjective: wrapAdjective
+  };
+  
